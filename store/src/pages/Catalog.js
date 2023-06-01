4015dcd584ff41679a38389ef854d5943e95fcb0
Dmitriy Lushin <dmas.git2022@gmail.com>
2023-06-02 02:38:04 +0300

    skeleton

--- store/src/pages/Catalog.js
+++ store/src/pages/Catalog.js
@@ -5,6 +5,9 @@ import CardProduct from "../components/card-product/CardProduct";
 const Catalog = () => {
     return (
         <div className="elements">
+            <h2>Наушники</h2>
+            <CardProduct />
+            <h2>Беспроводные наушники</h2>
             <CardProduct />
         </div>      
     )
