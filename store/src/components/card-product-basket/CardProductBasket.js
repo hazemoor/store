39d4c3c4215d2f0d5d96125df9be625cfe77aa58
Dmitriy Lushin <dmas.git2022@gmail.com>
2023-06-15 00:48:22 +0300

    basket with cards and total price

--- store/src/components/card-product-basket/CardProductBasket.js
+++ store/src/components/card-product-basket/CardProductBasket.js
@@ -1,13 +1,22 @@
 import React from "react";
 
-const CardProductBasket = () => {
+const CardProductBasket = ({ itemBasket }) => {
+    const {id, img, title, price, sale, rate} = itemBasket
     return (
+        
         <div className="card-basket">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+                <img src={img} />    
+            </div>
+            <div>{title}</div>
+            <div>{price}</div>
+            <div>{sale}</div>
+            <div>{rate}</div>
+            <button>-</button>
+            <span>1</span>
+            <button>+</button>
+            <button>delete</button>
+
         </div>
     )
 }
--- store/src/pages/Basket.js
+++ store/src/pages/Basket.js
@@ -1,15 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-import CardProduct from "../components/card-product/CardProduct";
+import CardProductBasket from "../components/card-product-basket/CardProductBasket";
 
 const Basket = () => {
+    const cartItems = useSelector(state => state.cart.cart)
+    const totalPrice = useSelector(state => state.cart.totalPrice)
     return (
         <div className="main">
             <div className="section-name">
                 <h2>
                     Корзина
                 </h2>
+                
             </div>
+            <div className="cards-price">
+                <li className="cards-basket">
+                    {cartItems.map((itemBasket) => (
+                        <CardProductBasket key={itemBasket.id} itemBasket={itemBasket} />
+                    ))}
+                </li>
+                <div className="total-price">
+                    <div>ИТОГО</div>
+                    <span>{totalPrice}</span>
+                </div>
+            </div>
+            
         </div>   
     )
 }
